d7e4015bef58515ab315bd30ee4ec9ce690b6bbc
gololobov <gololobov.serhiy@gmail.com>
2026-04-29 20:37:34 +0300

    29.04.2026

---
+++ AppCount/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+
+namespace AppCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string subKey = @"Software\RunCounter";
+            const string valueName = "RunCount";
+            const int freeRunsLimit = 18;
+
+            using var key = Registry.CurrentUser.CreateSubKey(subKey, writable: true);
+            object? raw = key.GetValue(valueName, 0);
+
+            int runCount;
+            if (raw is int i) runCount = i;
+            else if (raw is string s && int.TryParse(s, out var p)) runCount = p;
+            else
+            {
+                try { runCount = Convert.ToInt32(raw); }
+                catch { runCount = 0; }
+            }
+
+
+            if (args.Length > 0 && int.TryParse(args[0], out int manualSetCount))
+            {
+                manualSetCount = Math.Max(manualSetCount, 0);
+                runCount = manualSetCount;
+                key.SetValue(valueName, runCount, RegistryValueKind.DWord);
+                Console.WriteLine($"Счётчик вручную установлен: {runCount}");
+                return;
+            }
+
+
+            if (runCount < freeRunsLimit)
+            {
+                runCount = runCount + 1;
+                key.SetValue(valueName, runCount, RegistryValueKind.DWord);
+                int remaining = Math.Max(freeRunsLimit - runCount, 0);
+                Console.WriteLine($"Программа запущена. Всего запусков: {runCount}. Бесплатных запусков осталось: {remaining}.");
+                return;
+            }
+
+            // Бесплатные запуски исчерпаны
+            Console.WriteLine($"Бесплатные запуски исчерпаны (лимит {freeRunsLimit}). Таки платить надо чтоб запускать)) .");
+            Console.WriteLine($"Всего запусков: {runCount}.");
+        }
+    }
+}
---
+++ RegestryTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+
+namespace RegestryTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RegistryKey registryKey = Registry.CurrentUser;
+            //RegistryKey soft = registryKey.OpenSubKey("Software", true);
+            //RegistryKey myApp = soft.CreateSubKey("MyApp");
+            //myApp.SetValue("AppName", "RegestryTest");
+            //myApp.SetValue("user", "admin");
+            //myApp.SetValue("password", "12345");
+            //myApp.Close();
+
+
+
+            //RegistryKey registryKey = Registry.CurrentUser;
+            //RegistryKey soft = registryKey.OpenSubKey("Software\\MyApp", true);
+            //RegistryKey myApp = soft.CreateSubKey("Settings", true);
+            //myApp.SetValue("Color", "Red");
+            //myApp.Close();
+
+
+
+            //RegistryKey registryKey = Registry.CurrentUser;
+            //RegistryKey soft = registryKey.OpenSubKey("Software\\MyApp\\Settings", true);
+            //string color = soft.GetValue("Color").ToString();
+            //Console.BackgroundColor = color switch
+            //{
+            //    "Red" => ConsoleColor.Red,
+            //    "Green" => ConsoleColor.Green,
+            //    "Blue" => ConsoleColor.Blue,
+            //    _ => ConsoleColor.Black // Default case
+            //};
+            //Console.Clear();
+
+
+            //RegistryKey registryKey = Registry.CurrentUser;
+            //RegistryKey key = registryKey.OpenSubKey("Software\\MyApp\\Settings", true);
+            //key.DeleteValue("Color");
+            //key.Close();
+
+
+            RegistryKey registryKey = Registry.CurrentUser;
+            RegistryKey key = registryKey.OpenSubKey("Software\\MyApp", true);
+            key.DeleteSubKey("Settings");
+            key.Close();
+
+
+
+            //Напишите программу, которая будет подсчитывать количество собственных
+            //запусков, и когда параметр реестра достигнет 0, программа перестанет запускаться.Так
+            //же добавьте в программу возможность самостоятельно установить параметр, в реестре
+            //отвечающий за количество запусков.
+
+
+            Console.ReadLine();
+        }
+    }
+}
